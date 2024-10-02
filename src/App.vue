6a8bc28c918f020513f17439508f3a548550024b
Anthony Prospero <bellerophon77@live.fr>
2024-10-02 16:45:39 +0200

    clean files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -11,8 +7,12 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <MainLayout />
 </template>
+
+<script setup lang="ts">
+import MainLayout from "./components/views/MainLayout.vue";
+</script>
 
 <style scoped>
 .logo {
